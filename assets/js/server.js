779934fd19e1cf7602d1be9fdd9b80e67778257c
bellsofaba <bellsofaba@gmail.com>
2023-09-03 05:06:53 -0400

    nodemailer

---
+++ assets/js/server.js
@@ -0,0 +1,48 @@
+require('dotenv').config(); // Load environment variables from .env file
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+// Middleware to parse JSON and form data
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Nodemailer configuration using environment variables
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: process.env.EMAIL_USER, // Use the environment variable
+    pass: process.env.EMAIL_PASS, // Use the environment variable
+  },
+});
+
+// Define a route to handle form submissions
+app.post('/send-email', (req, res) => {
+  const { name, email, message } = req.body;
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER, // Use the environment variable
+    to: 'bellsofaba@gmail.com', // Update with recipient's email
+    subject: 'Contact Form Submission',
+    text: `Name: ${name}\nEmail: ${email}\nMessage:\n${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+      res.status(500).send('Error sending email');
+    } else {
+      console.log('Email sent:', info.response);
+      res.status(200).send('Email sent successfully');
+    }
+  });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
